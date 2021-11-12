3c207fde6b645ce6810a8ffd491a4bbe05911203
manchala meherish <manchala.meherish1524@gmail.com>
2021-11-12 16:07:38 +0530

    Update __init__.py

--- tkinter/__init__.py
+++ tkinter/__init__.py
@@ -34,7 +34,7 @@ import enum
 import sys
 import types
 
-import _tkinter # If this fails your Python may not be configured for Tk
+from tkinter import _tkinter # If this fails your Python may not be configured for Tk
 TclError = _tkinter.TclError
 from tkinter.constants import *
 import re
